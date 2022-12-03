f55963bca146fddfd2595417458d3a9a0d30d727
ade ishs <ade@adeishs.com>
2022-12-03 23:10:53 +1100

    Refactor

--- 03/p1.exs
+++ 03/p1.exs
@@ -1,18 +1,20 @@
 #! /usr/bin/env elixir
 
-get_items = fn r, len, half ->
-  MapSet.new(to_charlist(String.slice(r, half * len, len)))
-end
-
 IO.read(:stdio, :all)
 |> String.split("\n", trim: true)
 |> Enum.map(fn r ->
   len = div(String.length(r), 2)
-  c = [0, 1] |> Enum.map(fn half -> get_items.(r, len, half) end)
 
   i =
     Enum.at(
-      MapSet.intersection(Enum.at(c, 0), Enum.at(c, 1)) |> MapSet.to_list(),
+      Enum.reduce(
+        [0, len]
+        |> Enum.map(fn ofs ->
+          MapSet.new(to_charlist(r |> String.slice(ofs, len)))
+        end),
+        fn c, acc -> MapSet.intersection(acc, c) end
+      )
+      |> MapSet.to_list(),
       0
     )
 
