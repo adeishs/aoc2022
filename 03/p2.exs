97aed371b65f2ef515ad0ccbc2cb5db9db6d56b9
ade ishs <ade@adeishs.com>
2022-12-03 17:57:20 +1100

    Add part 2

---
+++ 03/p2.exs
@@ -0,0 +1,24 @@
+#! /usr/bin/env elixir
+
+IO.read(:stdio, :all)
+|> String.split("\n", trim: true)
+|> Enum.chunk_every(3)
+|> Enum.map(fn rs ->
+  cs = rs |> Enum.map(fn r -> MapSet.new(to_charlist(r)) end)
+
+  i =
+    Enum.at(
+      Enum.reduce(cs, fn c, acc -> MapSet.intersection(acc, c) end)
+      |> MapSet.to_list(),
+      0
+    )
+
+  1 +
+    cond do
+      i >= ?a && i <= ?z -> i - ?a
+      i >= ?A && i <= ?Z -> i - ?A + 26
+      true -> 0
+    end
+end)
+|> Enum.sum()
+|> IO.puts()
