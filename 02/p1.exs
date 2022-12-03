d68a06d925379a7fcd366604520070d1e293218a
ade ishs <ade@adeishs.com>
2022-12-04 00:11:23 +1100

    Use calculations instead of lookup table

--- 02/p1.exs
+++ 02/p1.exs
@@ -1,20 +1,22 @@
-#!/usr/bin/env elixir
+#! /usr/bin/env elixir
 
 IO.read(:stdio, :all)
 |> String.split("\n", trim: true)
 |> Enum.map(fn g ->
-  case g do
-    "A X" -> 1 + 3
-    "A Y" -> 2 + 6
-    "A Z" -> 3 + 0
-    "B X" -> 1 + 0
-    "B Y" -> 2 + 3
-    "B Z" -> 3 + 6
-    "C X" -> 1 + 6
-    "C Y" -> 2 + 0
-    "C Z" -> 3 + 3
-    _ -> 0
-  end
+  [opponent, play] =
+    g
+    |> String.split(" ", trim: true)
+    |> Enum.map(fn s -> to_charlist(s) |> Enum.at(0) end)
+
+  opponent = opponent - ?A
+  play = play - ?X
+
+  1 + play +
+    cond do
+      play == opponent -> 3
+      (play - opponent) in [1, -2] -> 6
+      true -> 0
+    end
 end)
 |> Enum.sum()
 |> IO.puts()
--- 02/p2.exs
+++ 02/p2.exs
@@ -1,34 +1,24 @@
-#!/usr/bin/env elixir
-
-outcomes = fn -> ["X", "Y", "Z"] end
+#! /usr/bin/env elixir
 
 IO.read(:stdio, :all)
 |> String.split("\n", trim: true)
 |> Enum.map(fn g ->
-  [opponent, outcome] = g |> String.split(" ", trim: true)
+  [opponent, outcome] =
+    g
+    |> String.split(" ", trim: true)
+    |> Enum.map(fn s -> to_charlist(s) |> Enum.at(0) end)
 
-  played_shape =
-    Enum.at(
-      outcomes.(),
-      rem(
-        Enum.find_index(outcomes.(), fn o -> o == outcome end) +
-          Enum.find_index(["B", "C", "A"], fn o -> o == opponent end) + 3,
-        3
-      )
-    )
+  opponent = opponent - ?A
+  play = rem(3 + opponent + outcome - ?X - 1, 3)
 
-  case "#{opponent} #{played_shape}" do
-    "A X" -> 1 + 3
-    "A Y" -> 2 + 6
-    "A Z" -> 3 + 0
-    "B X" -> 1 + 0
-    "B Y" -> 2 + 3
-    "B Z" -> 3 + 6
-    "C X" -> 1 + 6
-    "C Y" -> 2 + 0
-    "C Z" -> 3 + 3
-    _ -> 0
-  end
+  1 + play +
+    cond do
+      play == opponent -> 3
+      (play - opponent) in [1, -2] -> 6
+      true -> 0
+    end
 end)
 |> Enum.sum()
 |> IO.puts()
+
+# 10238
