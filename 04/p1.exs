84fc4564f89e7dca56bd81756ab38f4e0cab2f8a
ade ishs <ade@adeishs.com>
2022-12-04 16:34:06 +1100

    Add day 4 part 1

---
+++ 04/p1.exs
@@ -0,0 +1,30 @@
+#! /usr/bin/env elixir
+
+IO.read(:stdio, :all)
+|> String.split("\n", trim: true)
+|> Enum.map(fn a ->
+  sections =
+    a
+    |> String.split(",", trim: true)
+    |> Enum.map(fn r ->
+      [s, e] =
+        r
+        |> String.split("-", trim: true)
+        |> Enum.map(&String.to_integer/1)
+
+      s..e
+      |> Enum.to_list()
+      |> MapSet.new()
+    end)
+
+  cond do
+    MapSet.subset?(sections |> Enum.at(0), sections |> Enum.at(1)) ||
+        MapSet.subset?(sections |> Enum.at(1), sections |> Enum.at(0)) ->
+      1
+
+    true ->
+      0
+  end
+end)
+|> Enum.sum()
+|> IO.puts()
