04280bce1eacb7e345f6d61b6e8b9c3a08c01808
ade ishs <ade@adeishs.com>
2022-12-05 20:30:20 +1100

    Add day 5 part 1

---
+++ 05/p1.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+layout, insts = $stdin.read.split("\n\n")
+
+stacks = layout.split("\n")
+               .reverse
+               .drop(1)
+               .map { |l| (0..9).map { |i| l[4 * i + 1] } }
+               .transpose
+               .map { |s| s.reject { |b| b.nil? || b == ' ' } }
+               .reject(&:empty?)
+
+insts.split("\n")
+     .map do |inst|
+       _, n, _, src, _, dest = inst.split
+
+       src = src.to_i - 1
+       dest = dest.to_i - 1
+       n = n.to_i
+       move = stacks[src].pop(n).reverse
+       stacks[dest] = stacks[dest] + move
+     end
+
+puts stacks.map(&:pop).join
