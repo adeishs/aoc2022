f1419a8c8a726b89f375d4a5b505c4fdabfc9cd7
ade ishs <ade@adeishs.com>
2022-12-05 16:02:25 +1100

    Shorten code

--- 04/p2.exs
+++ 04/p2.exs
@@ -2,12 +2,11 @@
 
 IO.read(:stdio, :all)
 |> String.split("\n", trim: true)
-|> Enum.filter(fn a ->
+|> Enum.count(fn a ->
   [s0l, s0u, s1l, s1u] =
     Regex.split(~r{[,-]}, a)
     |> Enum.map(&String.to_integer/1)
 
   not (s0u < s1l || s0l > s1u || s1u < s0l || s1l > s0u)
 end)
-|> Enum.count()
 |> IO.puts()
