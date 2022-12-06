9fa48b4ce9d603914f455ec882e1d80de7b1584c
ade ishs <ade@adeishs.com>
2022-12-06 16:24:37 +1100

    Add part 2

---
+++ 06/p2.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+buffer = $stdin.readline.chomp.split('')
+SLICE_LEN = 14
+i = 0
+
+loop do
+  break if buffer[0...SLICE_LEN].uniq.size == SLICE_LEN
+
+  i += 1
+  buffer.shift
+end
+
+puts (i + SLICE_LEN).to_s
