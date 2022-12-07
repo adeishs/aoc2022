daa43be9b37a22e29c2253dade47936fb7ff67f9
ade ishs <ade.ishs@logicly.com.au>
2022-12-08 10:10:25 +1100

    Add part 2

---
+++ 07/p2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+subdirs = [0]
+sums = []
+$stdin.each_line do |cmd|
+  next if cmd == '$ cd /'
+
+  toks = cmd.split
+  next if toks[1] == 'ls' || toks[0] == 'dir'
+
+  if toks[0] != '$'
+    subdirs.append(subdirs.pop + toks[0].to_i)
+  elsif toks[1] == 'cd'
+    parent = toks.pop == '..'
+
+    if parent
+      sums.append(subdirs.pop)
+      subdirs.append(subdirs.pop + sums[sums.size - 1])
+    else
+      subdirs.append(0)
+    end
+  end
+end
+
+sds = subdirs.sum
+puts (sums + subdirs).select { |x| x > sds - 40_000_000 }.min
