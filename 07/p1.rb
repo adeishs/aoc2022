9fc180f619a1c6e13e9fad28ba273c93fb401e78
ade ishs <ade.ishs@logicly.com.au>
2022-12-08 10:02:27 +1100

    Add day 7 part 1

--- 07/p1.rb
+++ 07/p1.rb
@@ -1,9 +1,24 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-def traverse_dir()
+subdirs = [0]
+sums = []
+$stdin.each_line do |cmd|
+  next if cmd == '$ cd /'
+
+  toks = cmd.split
+  next if toks[1] == 'ls' || toks[0] == 'dir'
+
+  if toks[0] != '$'
+    subdirs.append(subdirs.pop + toks[0].to_i)
+  elsif toks[1] == 'cd'
+    if toks[2] == '..'
+      sums.append(subdirs.pop)
+      subdirs.append(subdirs.pop + sums[sums.size - 1])
+    else
+      subdirs.append(0)
+    end
+  end
 end
 
-$stdin.each_line do |cmd|
-  next if cmd == "$ cd /"
-end
+puts (sums + subdirs).select { |x| x <= 100_000 }.sum
