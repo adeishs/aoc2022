8a791cecaf05ac40537147f5b426484938ac7c27
ade ishs <ade@adeishs.com>
2022-12-13 22:58:28 +1100

    Make it more reusable for part 2

--- 13/p1.rb
+++ 13/p1.rb
@@ -4,21 +4,23 @@
 require 'json'
 
 def compare_pair(a, b)
+  return 0 if a.empty? && b.empty?
+
   i = 0
   loop do
-    return true if a[i].nil?
-    return false if b[i].nil?
+    return -1 if a[i].nil?
+    return 1 if b[i].nil?
 
     if !a[i].kind_of?(Array) && !b[i].kind_of?(Array) then
-      return true if a[i] < b[i]
-      return false if a[i] > b[i]
+      cmp = a[i] <=> b[i]
     else
       a[i] = [a[i]] unless a[i].kind_of?(Array)
       b[i] = [b[i]] unless b[i].kind_of?(Array)
 
-      return compare_pair(a[i], b[i]) unless a[i].empty? && b[i].empty?
+      cmp = compare_pair(a[i], b[i])
     end
 
+    return cmp unless cmp.zero?
     i += 1
   end
 end
@@ -27,5 +29,5 @@ puts $stdin.read
            .split("\n\n")
            .map { |pair_str| pair_str.split("\n").map { |v| JSON.parse(v) } }
            .map { |a, b| compare_pair(a, b) }
-           .map.with_index { |correct, i| if correct then i + 1 else 0 end }
+           .map.with_index { |cmp, i| if cmp == -1 then i + 1 else 0 end }
            .reduce(&:+)
