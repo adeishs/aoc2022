799c0eccc84964f0cb0cc9ceedc86e13c5de351e
ade ishs <ade@adeishs.com>
2022-12-13 23:18:00 +1100

    WIP part 2 — works with example, but not with my input

---
+++ 13/p2.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'json'
+
+DISTRESS_SIGNALS = ['[[2]]', '[[6]]']
+
+def compare_pair(a, b)
+  return 0 if a.empty? && b.empty?
+
+  i = 0
+  loop do
+    return -1 if a[i].nil?
+    return 1 if b[i].nil?
+
+    if !a[i].kind_of?(Array) && !b[i].kind_of?(Array) then
+      cmp = a[i] <=> b[i]
+    else
+      a[i] = [a[i]] unless a[i].kind_of?(Array)
+      b[i] = [b[i]] unless b[i].kind_of?(Array)
+
+      cmp = compare_pair(a[i], b[i])
+    end
+
+    return cmp unless cmp.zero?
+    i += 1
+  end
+end
+
+def distress(signal, i)
+  return i + 1 if DISTRESS_SIGNALS.include?(signal)
+  return 1
+end
+
+signals = DISTRESS_SIGNALS + $stdin.read.split("\n").reject { |l| l.empty? }
+puts signals.sort { |a, b| compare_pair(JSON.parse(a), JSON.parse(b)) }
+            .map.with_index { |signal, i| distress(signal, i) }
+            .reduce(&:*)
