5b23a5f02fe43c3239dffd9a23b5432744b28e9f
ade ishs <ade@adeishs.com>
2022-12-16 16:00:41 +1100

    Use ranges to make it easier to read

--- 15/p2.rb
+++ 15/p2.rb
@@ -30,21 +30,21 @@ def find_distress(sbs)
   (0..SCAN_MAX).each do |ty|
     exists = sbs.map { |sb| [sb[:S].real, sb[:dist] - (ty - sb[:S].imag).abs] }
                 .select { |_sx, rem| rem >= 0 }
-                .map { |sx, rem| { s: sx - rem, e: sx + rem } }
-                .sort { |a, b| a[:s] <=> b[:s] || a[:e] <=> b[:e] }
+                .map { |sx, rem| (sx - rem..sx + rem) }
+                .sort { |a, b| a.begin <=> b.begin || a.end <=> b.end }
 
     adjs = [exists.shift]
     until exists.size.zero?
-      if adjs.last[:e] >= exists[0][:s]
-        adjs[-1][:e] = exists[0][:e] if exists[0][:e] >= adjs.last[:e]
+      if adjs.last.end >= exists[0].begin
+        adjs[-1] = adjs[-1].begin..exists[0].end if exists[0].end >= adjs.last.end
         exists.shift
       else
         adjs << exists.shift
       end
     end
 
-    return Complex(adjs[0][:e] + 1, ty) if adjs.size == 2 &&
-                                           adjs[0][:e].between?(0, SCAN_MAX)
+    return Complex(adjs[0].end + 1, ty) if adjs.size == 2 &&
+                                           adjs[0].end.between?(0, SCAN_MAX)
   end
 end
 
