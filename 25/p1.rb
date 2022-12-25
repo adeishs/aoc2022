058b7aa6e599a7e112196ed63c751a79766a57fa
ade ishs <ade@adeishs.com>
2022-12-25 19:55:48 +1100

    Shorten code

--- 25/p1.rb
+++ 25/p1.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 SNAFU_DEC = %w[= - 0 1 2].map.with_index { |s, i| [s, i - 2] }.to_h
-DEC_SNAFU = SNAFU_DEC.map { |k, v| [v, k] }.to_h
+DEC_SNAFU = SNAFU_DEC.keys.rotate(2)
 
 def snafu_to_dec(snafu)
   snafu.reverse
@@ -13,17 +13,12 @@ end
 
 def dec_to_snafu(dec)
   snafu = []
-  p = 5 ** Math.log(dec, 5).floor
-  until p < 1
-    d = (dec <=> 0) * (1.0 * dec.abs / p).round
-
-    snafu << DEC_SNAFU[d]
-
-    dec -= d * p
-    p /= 5
+  until dec.zero?
+    snafu << DEC_SNAFU[dec % 5]
+    dec = dec / 5 + (%w[= -].include?(snafu.last) && 1 || 0)
   end
 
-  snafu.join('')
+  snafu.reverse.join('')
 end
 
 puts dec_to_snafu($stdin.each_line
