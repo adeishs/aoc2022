e36a1212c02486e7b24cdb00047f57ef747f4e52
ade ishs <ade@adeishs.com>
2023-10-31 11:51:12 +1100

    Refactor to use Set

--- 18/p1.rb
+++ 18/p1.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-cube = Hash[*$stdin.each_line
-                   .map { |line| line.chomp.split(',').map(&:to_i) }
-                   .flat_map { |k| [k, true] }]
+require 'set'
 
-puts cube.map(&:flatten).map { |x, y, z, _|
+cube = Set[*$stdin.each_line
+                  .map { |line| line.chomp.split(',').map(&:to_i) }]
+
+puts cube.map(&:flatten).map { |x, y, z|
   [-1, 1].map do |d|
     [[x + d, y, z],
      [x, y + d, z],
-     [x, y, z + d]].reject { |c| cube[c] }.size
+     [x, y, z + d]].reject { |c| cube.member?(c) }.size
   end.sum
 }.sum
