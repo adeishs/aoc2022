9c4e9c76971521515c5289cebede5e8e66db8d40
ade ishs <ade@adeishs.com>
2023-10-31 21:15:19 +1100

    Solve day 18 part 2

---
+++ 18/p2.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+def get_air(airs, lavas, x, y, z)
+  return if airs.member?([x, y, z]) || lavas.member?([x, y, z])
+
+  airs.add([x, y, z])
+  get_air(airs, lavas, x - 1, y, z)
+  get_air(airs, lavas, x + 1, y, z)
+  get_air(airs, lavas, x, y - 1, z)
+  get_air(airs, lavas, x, y + 1, z)
+  get_air(airs, lavas, x, y, z - 1)
+  get_air(airs, lavas, x, y, z + 1)
+end
+
+MAX = 20
+lavas = Set[*$stdin.each_line
+                   .map { |line| line.chomp.split(',').map(&:to_i) }]
+airs = Set.new
+
+(-1..MAX).each do |i|
+  (-1..MAX).each do |j|
+    [-1, MAX].each { |k| airs.merge([[k, i, j], [i, k, j], [i, j, k]]) }
+  end
+end
+
+get_air(airs, lavas, 0, 0, 0)
+
+puts lavas.map(&:flatten).map { |x, y, z|
+  (airs.member?([x - 1, y, z]) ? 1 : 0) +
+    (airs.member?([x + 1, y, z]) ? 1 : 0) +
+    (airs.member?([x, y - 1, z]) ? 1 : 0) +
+    (airs.member?([x, y + 1, z]) ? 1 : 0) +
+    (airs.member?([x, y, z - 1]) ? 1 : 0) +
+    (airs.member?([x, y, z + 1]) ? 1 : 0)
+}.sum
