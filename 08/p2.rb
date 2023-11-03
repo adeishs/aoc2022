625cda00ea6a5f9730dc51efaca437f641701a43
ade ishs <ade@adeishs.com>
2023-11-03 22:56:09 +1100

    Solve day 8 part 2

--- 08/p2.rb
+++ 08/p2.rb
@@ -6,21 +6,43 @@ heights = $stdin.read.split("\n").map { |row| row.split('').map(&:to_i) }
 max_row = heights.size - 1
 max_col = heights[0].size - 1
 
-scenic_scores = (1...max_row).to_a
-                             .product((1...max_col).to_a)
-                             .map do |row, col|
-  [
-    (row - 1..0).step(-1)
-                .map { |i| [heights[i][col], heights[i + 1][col]] }
-                .index { |curr, prev| curr >= prev } || (row - 1),
-    (row + 1..max_row).map { |i| [heights[i][col], heights[i - 1][col]] }
-                      .index { |curr, prev| curr >= prev } || (max_row - row),
-    (col - 1..0).step(-1)
-                .map { |i| [heights[row][i], heights[row][i + 1]] }
-                .index { |curr, prev| curr >= prev } || (col - 1),
-    (col + 1..max_col).map { |i| [heights[row][i], heights[row][i - 1]] }
-                      .index { |curr, prev| curr >= prev } || (max_col - col)
-  ].reduce { |acc, d| acc * (d + 1) }
-end
+highest_scenic_score =
+  (1...max_row).to_a.product((1...max_col).to_a).map do |row, col|
+    visible_counts = []
 
-puts scenic_scores.max
+    c = 1
+    (col - 1).downto(1).each do |x|
+      break if heights[row][x] >= heights[row][col]
+
+      c += 1
+    end
+    visible_counts.push(c)
+
+    c = 1
+    (col + 1...max_col).each do |x|
+      break if heights[row][x] >= heights[row][col]
+
+      c += 1
+    end
+    visible_counts.push(c)
+
+    c = 1
+    (row - 1).downto(1).each do |y|
+      break if heights[y][col] >= heights[row][col]
+
+      c += 1
+    end
+    visible_counts.push(c)
+
+    c = 1
+    (row + 1...max_row).each do |y|
+      break if heights[y][col] >= heights[row][col]
+
+      c += 1
+    end
+    visible_counts.push(c)
+
+    visible_counts.reduce(1, :*)
+  end.max
+
+puts highest_scenic_score
