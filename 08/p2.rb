b4881bbee4b575a1be1e4992dec38a4ff36e827e
ade ishs <ade@adeishs.com>
2023-11-04 06:32:03 +1100

    DRY

--- 08/p2.rb
+++ 08/p2.rb
@@ -7,42 +7,19 @@ max_row = heights.size - 1
 max_col = heights[0].size - 1
 
 highest_scenic_score =
-  (1...max_row).to_a.product((1...max_col).to_a).map do |row, col|
-    visible_counts = []
+  [*1...max_row].product([*1...max_col]).map do |row, col|
+    count = lambda do |line_heights|
+      line_heights.each_with_index do |h, i|
+        return i + 1 if h >= heights[row][col]
+      end
 
-    c = 1
-    (col - 1).downto(1).each do |x|
-      break if heights[row][x] >= heights[row][col]
-
-      c += 1
+      line_heights.size + 1
     end
-    visible_counts.push(c)
 
-    c = 1
-    (col + 1...max_col).each do |x|
-      break if heights[row][x] >= heights[row][col]
-
-      c += 1
-    end
-    visible_counts.push(c)
-
-    c = 1
-    (row - 1).downto(1).each do |y|
-      break if heights[y][col] >= heights[row][col]
-
-      c += 1
-    end
-    visible_counts.push(c)
-
-    c = 1
-    (row + 1...max_row).each do |y|
-      break if heights[y][col] >= heights[row][col]
-
-      c += 1
-    end
-    visible_counts.push(c)
-
-    visible_counts.reduce(1, :*)
+    count.call((col - 1).downto(1).map { |x| heights[row][x] }) *
+      count.call((col + 1...max_col).map { |x| heights[row][x] }) *
+      count.call((row - 1).downto(1).map { |y| heights[y][col] }) *
+      count.call((row + 1...max_row).map { |y| heights[y][col] })
   end.max
 
 puts highest_scenic_score
