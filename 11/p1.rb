d19d18238b09a835d01d8f0a45cab55c8eb60c91
ade ishs <ade@adeishs.com>
2023-11-04 09:40:44 +1100

    Convert to OO

--- 11/p1.rb
+++ 11/p1.rb
@@ -1,6 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+class Monkey
+  attr_accessor :items
+  attr_accessor :operator
+  attr_accessor :operand
+  attr_accessor :test_div
+  attr_accessor :target
+  attr_accessor :inspect_count
+end
+
 def parse_monkey(monkey_spec)
   lines = monkey_spec.split("\n").map(&:strip)
 
@@ -8,18 +17,19 @@ def parse_monkey(monkey_spec)
   _ = lines.shift
 
   _, tmp = lines.shift.split(': ')
-  monkey = { items: tmp.split(', ').map(&:to_i) }
+  monkey = Monkey.new
+  monkey.items = tmp.split(', ').map(&:to_i)
 
-  monkey[:operator], tmp = lines.shift.sub('Operation: new = old ', '').split
-  monkey[:operand] = tmp == 'old' ? nil : tmp.to_i
+  monkey.operator, tmp = lines.shift.sub('Operation: new = old ', '').split
+  monkey.operand = tmp == 'old' ? nil : tmp.to_i
 
-  monkey[:test_div] = lines.shift.sub('Test: divisible by ', '').to_i
-  monkey[:target] = {
+  monkey.test_div = lines.shift.sub('Test: divisible by ', '').to_i
+  monkey.target = {
     true => lines.shift.sub('If true: throw to monkey ', '').to_i,
     false => lines.shift.sub('If false: throw to monkey ', '').to_i
   }
 
-  monkey[:inspect_count] = 0
+  monkey.inspect_count = 0
 
   monkey
 end
@@ -28,22 +38,21 @@ monkeys = $stdin.read.split("\n\n").map { |spec| parse_monkey(spec) }
 
 20.times do
   monkeys.each do |monkey|
-    monkey[:inspect_count] += monkey[:items].size
+    monkey.inspect_count += monkey.items.size
 
-    monkey[:items].size.times do
-      worry_level = monkey[:items].shift
-
-      if monkey[:operator] == '+'
-        worry_level += monkey[:operand]
+    monkey.items.each do |worry_level|
+      if monkey.operator == '+'
+        worry_level += monkey.operand
       else
-        worry_level *= monkey[:operand] || worry_level
+        worry_level *= monkey.operand || worry_level
       end
       worry_level /= 3
 
-      target = monkey[:target][(worry_level % monkey[:test_div]).zero?]
-      monkeys[target][:items].push(worry_level)
+      target = monkey.target[(worry_level % monkey.test_div).zero?]
+      monkeys[target].items.push(worry_level)
     end
+    monkey.items = []
   end
 end
 
-puts monkeys.map { |m| m[:inspect_count] }.sort.pop(2).reduce(1, :*)
+puts monkeys.map { |m| m.inspect_count }.sort.pop(2).reduce(1, :*)
