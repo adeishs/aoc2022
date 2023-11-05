d77c059a6b68a75119211e20110361aabebe3b51
ade ishs <ade@adeishs.com>
2023-11-05 22:52:53 +1100

    Solve day 21 part 1

---
+++ 21/p1.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+class Monkey
+  attr_reader :name
+  attr_accessor :operator, :operands, :value
+
+  def initialize(name, args)
+    @name = name
+    if args.size == 1
+      @value = args[0]
+    else
+      @operator = args[1]
+      @operands = [args[0], args[2]]
+    end
+  end
+end
+
+def parse(line)
+  name, arg_str = line.split(' ', 2)
+  args = arg_str.split
+  args[0] = args[0].to_i if args.size == 1
+  name = name.sub(':', '')
+
+  [name, Monkey.new(name, args)]
+end
+
+def resolvable(monkey, value_monkey)
+  monkey.operands.all? { |o| value_monkey.include?(o) }
+end
+
+monkey = { operator: Hash[$stdin.each_line.map { |line| parse(line.chomp) }] }
+monkey[:value] = monkey[:operator].reject { |_, m| m.value.nil? }
+monkey[:operator].keep_if { |_, m| m.value.nil? }
+until monkey[:operator].empty?
+  monkey[:operator].select { |_, m| resolvable(m, monkey[:value]) }
+                   .each do |n, m|
+    op1 = monkey[:value][m.operands.shift].value
+    op2 = monkey[:value][m.operands.shift].value
+    m.value =
+      case m.operator
+      when '+'
+        op1 + op2
+      when '-'
+        op1 - op2
+      when '*'
+        op1 * op2
+      when '/'
+        op1 / op2
+      end
+    monkey[:value][n] = m
+    monkey[:operator].delete(n)
+  end
+end
+puts monkey[:value]['root'].value
